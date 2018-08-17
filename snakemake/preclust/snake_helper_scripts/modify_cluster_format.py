f7d75e078a144137ce5923e64e6dc26234600f2f
Kristoffer Sahlin <kxs624@psu.edu>
2018-08-17 09:41:33 +0200

    added evaluation part to snakemake

--- snakemake/preclust/snake_helper_scripts/modify_cluster_format.py
+++ snakemake/preclust/snake_helper_scripts/modify_cluster_format.py
@@ -1,33 +1,36 @@
 import argparse, os
 
-from collections import defaultdict
+# from collections import defaultdict
 
 
-def parse_inferred_clusters_tsv(tsv_file, args):
-    infile = open(tsv_file , "r")
-    clusters = {}
-    for line in infile:
-        cluster_id, read_acc = line.strip().split("\t")
-        if args.simulated:
-            read_acc = "_".join([item for item in read_acc.split("_")[:-1] ])
-        else:
-            read_acc = read_acc.split("_strand")[0] 
-
-        clusters[read_acc] = int(cluster_id)
-    return clusters
 
 def main(args):
+    center_to_cluster_mapping = {}
+    current_cl_index = 0
+    cluster_to_reads = {}
+    # reads_to_cluster = {}
+    for i, line in enumerate(open(args.input, "r")):
+        if i == 0: # header
+            continue 
+        vals = line.strip().split()
+        from_read_acc, to_read_acc = vals[0], vals[1]
 
-    clusters = parse_inferred_clusters_tsv(args.clusters, args)
-    if args.simulated:
-        ref_file = pysam.AlignmentFile(args.classes, "r", check_sq=False)
-        classes  = parse_true_clusters_simulated(ref_file)
-    else:
-        ref_file = pysam.AlignmentFile(args.classes, "rb", check_sq=False)
-        classes  = parse_true_clusters(ref_file)
+        if to_read_acc in center_to_cluster_mapping:
+            cluster_id = center_to_cluster_mapping[to_read_acc]
+            cluster_to_reads[cluster_id].append(from_read_acc)
+        else:
+            cluster_id = current_cl_index
+            cluster_to_reads[cluster_id] = [from_read_acc]
+            center_to_cluster_mapping[to_read_acc] = cluster_id
+            current_cl_index += 1
 
-    value = compute_V_measure(clusters, classes)
-    get_cluster_information(clusters, classes)
+    outfile = open(args.output, "w")
+    for cl_id, list_read_acc in cluster_to_reads.items():
+        assert len(list_read_acc) >= 1
+        for read_acc in list_read_acc:
+            outfile.write("{0}\t{1}\n".format(cl_id, read_acc))
+    outfile.close()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Align predicted transcripts to transcripts in ensembl reference data base.")
